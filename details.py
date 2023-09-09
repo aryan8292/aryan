8681c2027fdee8a604e0cc0fcc9bda7b8659ffe6
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2023-09-09 08:52:00 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
-api_id = 21609776
-api_hash = "61f2558d634df353a2a9806661f4b091"
-bot_token = "5940119184:AAHKEw7V7zGVZvwV4T4RCgmZKfR7lSGKNNE"
-auth_users = [5623849580,-1001861953997]
-sudo_user = 5623849580
-log_channel = -1001791480211
-txt_channel = -1001791480211
+api_id = 3334521
+api_hash = "29edd7420d528140c7a04bd47486886f"
+bot_token = "6411839474:AAG71ZmRY1ChT7o-XVZaXeoUEwi1WHPISfI"
+auth_users = [5079629749,-1001861953997]
+sudo_user = 5079629749
+log_channel = -1001932711037
+txt_channel = -1001624903439
